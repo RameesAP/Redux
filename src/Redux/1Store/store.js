ac86dca08c443e75adac15018e72e4dd53538c6b
RameesAP <rameeswork@gmail.com>
2023-03-08 20:31:50 +0530

    store created

---
+++ src/Redux/1Store/store.js
@@ -0,0 +1,9 @@
+import { legacy_createStore as createStore } from 'redux'
+// import { createStoreHook } from 'react-redux';
+
+
+import { allReducers } from '../3Reducer';
+
+let store = createStore(allReducers, window.REDUX_DEVTOOL_EXTENSION__ && window.REDUX_DEVTOOL_EXTENSION__())
+
+export default store
--- src/index.js
+++ src/index.js
@@ -3,14 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createStoreHook } from 'react-redux';
 
-let store = createStoreHook()
+
+import { Provider } from 'react-redux';
+import store from './Redux/1Store/store';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
