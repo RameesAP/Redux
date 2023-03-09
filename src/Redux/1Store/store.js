68256a3afaea6837c834ebd705f33988f38a3183
RameesAP <rameeswork@gmail.com>
2023-03-09 14:44:21 +0530

    extion issue scess

--- src/Redux/1Store/store.js
+++ src/Redux/1Store/store.js
@@ -3,6 +3,6 @@ import { legacy_createStore as createStore } from 'redux'
 
 import { allReducers } from '../3Reducer';
 
-let store = createStore(allReducers, window.REDUX_DEVTOOL_EXTENSION__ && window.REDUX_DEVTOOL_EXTENSION__())
+const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 export default store
